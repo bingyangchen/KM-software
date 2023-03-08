09c3cdaa56ff17f305a8d623fb86b97b571ae0bd
Jamison Chen <106208004@g.nccu.edu.tw>
2023-03-08 13:09:39 +0800

    Update

--- push.sh
+++ push.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-if [[ -z $1 ]]; then
-    echo Please provide the commit message.
-else
-    npx book sm
-    
-    git config user.name "Jamison Chen"
-    git config user.email "106208004@g.nccu.edu.tw"
-    git add .
-    git commit -m "$1"
+npx book sm
 
-    ssh-add -D
-    ssh-add ~/.ssh/id_rsa
-    git push -u origin master
+git config user.name "Jamison Chen"
+git config user.email "106208004@g.nccu.edu.tw"
+git add .
+
+if [[ -z $1 ]]; then
+    git commit -m "Update"
+else
+    git commit -m "$1"
 fi
+
+ssh-add -D
+ssh-add ~/.ssh/id_rsa
+git push -u origin master
