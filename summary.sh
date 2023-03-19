fc39f007a318b53eb4e481005e5bf198c47b3ef6
Jamison Chen <106208004@g.nccu.edu.tw>
2023-03-19 11:58:36 +0800

    Update

--- summary.sh
+++ summary.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Directories to ignore
+ignore_dirs=("node_modules" "圖庫" "Pinkoi")
+
+# Specific markdown files to ignore
+ignore_md_files=("./DEVNOTE.md" "./SUMMARY.md")
+
 # Recursive function to generate the GitBook summary file
 generate_summary() {
     local path="$1"
@@ -11,17 +17,16 @@ generate_summary() {
     fi
 
     for file in "$path"/*; do
-        # Files to ignore
-        if [[ "$file" == "./DEVNOTE.md" || "$file" == "./SUMMARY.md" ]]; then
-            continue
-        elif [[ -d "$file" && $(basename "$file") != "node_modules" && $(basename "$file") != "圖庫" ]]; then
+        if [[ -d "$file" && !("${ignore_dirs[@]}" =~ $(basename "$file")) ]]; then
             if [[ "$path" == "." ]]; then
                 generate_summary "$file" ""
             else
                 generate_summary "$file" "$indent  "
             fi
         elif [[ -f "$file" && $(basename "$file") == *.md ]]; then
-            echo "${indent}  - [$(basename "$file" .md)](<./$file>)" >>SUMMARY.md
+            if [[ !("${ignore_md_files[@]}" =~ "$file") ]]; then
+                echo "${indent}  - [$(basename "$file" .md)](<./$file>)" >>SUMMARY.md
+            fi
         fi
     done
 }
