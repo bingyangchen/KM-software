af77dc919f25739af8c760447e1542c2eea13f0c
Bryan Chen <bryan.chen.429@gmail.com>
2024-09-21 11:59:23 +0800

    Update push script

--- push.sh
+++ push.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$current_branch" != "dev" ]; then
-    echo "Error: You are not on the correct branch. Aborting."
+    printf "${RED}Error: You are not on the correct branch. Aborting.${RESET}\n"
     exit 1
 fi
 
@@ -16,9 +22,11 @@ else
 fi
 
 git push origin dev
+printf "${BLUE}------------ Remote branch 'dev' updated ------------${RESET}\n"
 
 git switch master
 git merge -Xtheirs dev -m "Merge branch 'dev'"
+printf "${BLUE}------ Local branch 'dev' merged into 'master' ------${RESET}\n"
 
 python -B summary.py
 python -B normalize_img_links.py
@@ -29,5 +37,7 @@ set +e
 git add -A
 git commit -m Update
 git push origin master
+printf "${BLUE}----------- Remote branch 'master' updated -----------${RESET}\n"
 
 git switch dev
+printf "${GREEN}Done!${RESET}\n"
